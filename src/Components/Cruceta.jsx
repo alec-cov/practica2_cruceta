59e3e7a6395c8f1fcdd5fa94398bcfede38504e2
alec-cov <80437740+alec-cov@users.noreply.github.com>
2022-03-30 00:38:23 -0700

    Cruceta working
    Done

--- src/Components/Cruceta.jsx
+++ src/Components/Cruceta.jsx
@@ -2,9 +2,33 @@ import "./Cruceta.css";
 export default function Cruceta1(){
     return(
         <>
-        <div className="cruceta1">
-
-        </div>
+	<div id="rojo2" ></div>
+	<div id="rojo2" style={{top:600}}></div>
+	<div id="verde2"style={{left:600}}></div>
+	<div id="verde2"></div>
+	<div id="verde"></div>
+	<div id="rojo"></div>
+	<div id="amarillo"></div>
+	<div id="azul"></div>
+	<div id="verde" style={{left:400}}></div>
+	<div id="rojo" style={{left:450}}></div>
+	<div id="amarillo" style={{left:500}}></div>
+	<div id="azul" style={{left:550}}></div>
+	<div id="verde" style={{top:400}}></div>
+	<div id="rojo" style={{top:450}}></div>
+	<div id="amarillo" style={{top:500}}></div>
+	<div id="azul" style={{top:550}}></div>
+	<div id="verde3"></div>
+	<div id="rojo3"></div>
+	<div id="amarillo2"></div>
+	<div id="azul2"></div>
+	<div id="morado"></div>
+	<div id="semicirculo"></div>
+	<div id="semicirculo2"></div>
+	<div id="semicirculo3"></div>
+	<div id="semicirculo4"></div>
+	<div id="circulo"></div>
+	<div id="circuloblanco"></div>
         </>
     );
 }
